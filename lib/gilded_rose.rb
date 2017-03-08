3f2117fbb978dc0b5258048c5a41bbeef2d8d96d
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-03-08 10:50:01 +0000

    Removed magic numbers

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -25,6 +25,16 @@ class Item
 end
 
 class ItemDefauts < Item
+  QualityMax = 50
+  QualityMin = 0
+  SellInMin = 0
+  SellInMax1 = 11
+  SellInMax2 = 6
+  IncreaseQualityDefault = 1
+  DecreaseQualityDefault = 1
+  DecreaseSellInDefault = 1
+  QualityAmount = 1
+
   def initialize(name, sell_in, quality)
     super(name, sell_in, quality)
   end
@@ -32,22 +42,22 @@ class ItemDefauts < Item
   def calculate
     self.quality_for_sale
     self.decrease_sell_in
-    self.quality_amount if self.sell_in < 0
+    self.quality_amount if self.sell_in < SellInMin
   end
 
-  def increase_quality(amount=1)
-    self.quality += amount if self.quality < 50
+  def increase_quality(amount=IncreaseQualityDefault)
+    self.quality += amount if self.quality < QualityMax
   end
 
-  def decrease_quality(amount=1)
-    self.quality -= amount if self.quality > 0
+  def decrease_quality(amount=DecreaseQualityDefault)
+    self.quality -= amount if self.quality > QualityMin
   end
 
-  def decrease_sell_in(amount=1)
+  def decrease_sell_in(amount=DecreaseSellInDefault)
     self.sell_in -= amount
   end
 
-  def quality_amount(amount=1)
+  def quality_amount(amount=QualityAmount)
     self.decrease_quality(amount)
   end
 
@@ -61,8 +71,8 @@ class Other < ItemDefauts
 end
 
 class Sulfuras < ItemDefauts
-  def decrease_sell_in(amount=1); end
-  def quality_amount(amount=1); end
+  def decrease_sell_in(amount=DecreaseSellInDefault); end
+  def quality_amount(amount=QualityAmount); end
 end
 
 class AgedBrie < ItemDefauts
@@ -72,17 +82,17 @@ class AgedBrie < ItemDefauts
 end
 
 class Backstage < ItemDefauts
-  def quality_amount(amount=1); end
+  def quality_amount(amount=QualityAmount); end
 
   def quality_for_sale
     self.increase_quality
-    self.increase_quality if self.sell_in < 11
-    self.increase_quality if self.sell_in < 6
+    self.increase_quality if self.sell_in < SellInMax1
+    self.increase_quality if self.sell_in < SellInMax2
   end
 end
 
 class Conjured < ItemDefauts
   def quality_for_sale
-    self.decrease_quality(2)
+    self.decrease_quality(QualityAmount*2)
   end
 end
